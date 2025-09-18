397b57849749e99eab2922e9bdcb269dda3c026e
cag2025 <cag.22ex23001@gmail.com>
2025-09-18 15:42:13 +0530

    Midsem Questions

---
+++ midsem/question5/plot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+Reg=-RIN
+Pro=-Jq0.6
+ps=answer.ps
+gmt gmtset MAP_FRAME_TYPE fancy
+CPT=aman.cpt
+GRD=~/Desktop/topo30.grd
+gmt grdcut $GRD $Reg -Gtmp1.grd -V
+gmt grdsample tmp1.grd -Gtmp.grd -I0.5m -r -nc+c -V
+#gmt grdmath -25 tmp1.grd MAX=tmp2.grd
+gmt grdgradient tmp.grd -A315 -Ggradient.grd -Nt -V
+gmt grdimage $Reg $Pro tmp.grd -C$CPT -K > $ps
+gmt pscoast $Reg $Pro -Df -W0.5p,black,dashed -Bf4g2 -Na/1p,black -I1/0.3p,blue -O -K >> $ps
+echo "82.2723 22.7284" | gmt psxy $Reg $Pro -Si3p -Gred -W5p,red -O -K >> $ps
+cat "location.dat" | awk '{print $2,$1}' | gmt psxy $Reg $Pro -Sc3p -Gblue -W4,blue -O -K >> $ps
+okular $ps
